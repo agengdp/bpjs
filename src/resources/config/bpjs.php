af9f25fe1c185b99c1edb51379127786f6d8d8cb
Ageng <rexcarnation@gmail.com>
2020-03-19 10:46:33 +0700

    add kode ppk

--- src/resources/config/bpjs.php
+++ src/resources/config/bpjs.php
@@ -4,5 +4,6 @@ return [
     'cons_id'       => env('BPJS_CONS_ID'),
     'secret_key'    => env('BPJS_SECRET_KEY'),
     'base_url'      => 'https://new-api.bpjs-kesehatan.go.id:8080/',
-    'service_name'  => 'new-vclaim-rest'
+    'service_name'  => 'new-vclaim-rest',
+    'ppk'           => env('BPJS_PPK')       
 ];
