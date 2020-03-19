03423a5ebbf71a74011e43d38d639a6327f2d46f
Ageng <rexcarnation@gmail.com>
2020-03-19 10:27:06 +0700

    update config

--- src/resources/config/bpjs.php
+++ src/resources/config/bpjs.php
@@ -1,10 +1,8 @@
 <?php 
 
 return [
-    'bpjs'          => [
-        'cons_id'       => env('BPJS_CONS_ID'),
-        'secret_key'    => env('BPJS_SECRET_KEY'),
-        'base_url'      => 'https://dvlp.bpjs-kesehatan.go.id',
-        'service_name'  => 'v-claim-rest'    
-    ]
+    'cons_id'       => env('BPJS_CONS_ID'),
+    'secret_key'    => env('BPJS_SECRET_KEY'),
+    'base_url'      => 'https://new-api.bpjs-kesehatan.go.id:8080/',
+    'service_name'  => 'new-vclaim-rest'
 ];
